05994150ea84c5c3e45f4dc303ed12c76734feb4
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-02-19 22:08:10 +0200

    update search

--- src/index.js
+++ src/index.js
@@ -7,14 +7,35 @@ document.querySelector('.live-search').oninput = function () {
   if (value != '') {
     searhItems.forEach(function (elem) {
       if (elem.innerText.search(value) === -1) {
-        elem.classList.add('hide');
+        hideClear(elem);
+        // elem.classList.add('hide');
+        // elem.innerHTML = elem.innerText;
       } else {
         elem.classList.remove('hide');
+        let str = elem.innerText;
+        elem.innerHTML = insertMark(str, elem.innerText.search(value), value.length);
       }
     });
   } else {
     searhItems.forEach(function (elem) {
+      // hideClear(elem);
       elem.classList.remove('hide');
+      elem.innerHTML = elem.innerText;
     });
   }
 };
+
+function insertMark(string, pos, len) {
+  return (
+    string.slice(0, pos) +
+    '<mark>' +
+    string.slice(pos, pos + len) +
+    '</mark>' +
+    string.slice(pos + len)
+  );
+}
+
+function hideClear(elem) {
+  elem.classList.add('hide');
+  elem.innerHTML = elem.innerText;
+}
