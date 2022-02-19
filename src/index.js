12e8495de23c3054ca58fa1ec1d36badcd63eb7a
Aleksandr Serebro <aleksandrserebroz@gmail.com>
2022-02-19 21:48:33 +0200

    add search

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 import './sass/main.scss';
+
+document.querySelector('.live-search').oninput = function () {
+  let value = this.value.trim();
+  let searhItems = document.querySelectorAll('.search li');
+  // console.log('~ searhItems', searhItems);
+  if (value != '') {
+    searhItems.forEach(function (elem) {
+      if (elem.innerText.search(value) === -1) {
+        elem.classList.add('hide');
+      } else {
+        elem.classList.remove('hide');
+      }
+    });
+  } else {
+    searhItems.forEach(function (elem) {
+      elem.classList.remove('hide');
+    });
+  }
+};
